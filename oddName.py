33bde3ac1c2766cefe28848daec523a9bd0877f7
OwenJMathieson <owenisthe1@yahoo.com.au>
2017-08-24 10:11:40 +1000

    :O

--- oddName.py
+++ oddName.py
@@ -1,9 +1,19 @@
-Name= str(input("Name:"))
+def main():
 
-while Name == "":
-    Name = input("Name:")
-
-print("{}".format(Name[::2]))
-
+    Name = get_name()
+    refactor = int(input("Insert the Split Factor:"))
+    print_name(Name, refactor)
 
 
+def print_name(Name, refactor):
+    print("{}".format(Name[::refactor]))
+
+
+def get_name():
+    Name = str(input("Name:"))
+    while Name == "":
+        Name = input("Name:")
+    return Name
+
+
+main()
